a31a4024d3acff6c48a879973a6ed347806535f8
Gonza <gonza.cunet@outlook.com>
2025-02-10 10:06:00 -0300

    fifth commit

--- src/helpers/getGif.ts
+++ src/helpers/getGif.ts
@@ -1,5 +1,5 @@
 export const getGifs = async (category: any) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${category}&api_key=ial1SonTjAJuJYEAt9vUM38SBVG1O8AV&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&api_key=ial1SonTjAJuJYEAt9vUM38SBVG1O8AV&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
